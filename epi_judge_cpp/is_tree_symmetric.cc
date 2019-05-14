7b2fe961f103a8002279df541fee2f66efaf6931
ibm <iamindrajitb@gmail.com>
2019-05-14 08:27:14 +0530

    Mirror Tree

--- epi_judge_cpp/is_tree_symmetric.cc
+++ epi_judge_cpp/is_tree_symmetric.cc
@@ -1,9 +1,20 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+bool check_and_mirror(const unique_ptr<BinaryTreeNode<int>>& tree, const unique_ptr<BinaryTreeNode<int>>& mirror_tree) {
+  if( tree == NULL && mirror_tree == NULL )
+    return true;
+  if(tree->data == mirror_tree->data) {
+    return (true && check_and_mirror(tree->left,mirror_tree->right) && check_and_mirror(tree->right, mirror_tree->left));
+  }
+  else return false;
+}
 
 bool IsSymmetric(const unique_ptr<BinaryTreeNode<int>>& tree) {
   // TODO - you fill in here.
-  return true;
+  if(tree == NULL)
+    return true;
+
+  return check_and_mirror(tree->right,tree->left);
 }
 
 int main(int argc, char* argv[]) {
